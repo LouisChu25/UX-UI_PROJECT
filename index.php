7552e3d786a0284db4eb10b01b10f3a769bc1411
Louis Chu <louis.chu@edu.devinci.fr>
2022-02-09 12:58:43 +0100

    header updated

--- index.php
+++ index.php
@@ -9,7 +9,22 @@
 </head>
 <body>
     <header>
-        
+        <div class="container1_header">
+            <img src="img/logo-IIM.png" alt="Logo IIM">
+            <h1>DEVENEZ UN <br> DEVELOPPEUR</h1>
+            <p>Création de site web, Programmation de 
+                jeux vidéos, Découverte de nouvelles 
+                technologies
+            </p>
+            <div class="button-container">
+                <div class="button-formation">
+                    <a href="#">Mini formations</a>
+                </div>
+                <div class="button-decouvrir">
+                    <a href="iim.fr">Découvrir l'IIM</a>
+                </div>
+            </div>
+        </div>
     </header>
 </body>
 </html>
