fbc8481883977cfb8a06f806933159b2ae88bfc8
lnourry75 <90520293+lnourry75@users.noreply.github.com>
2022-02-11 11:06:08 +0100

    tracking google tag manager and axeptio script

--- index.php
+++ index.php
@@ -19,6 +19,14 @@
   gtag('config', 'G-ZXHBCFXF79');
 </script>
 	
+	<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N5LK9VB');</script>
+<!-- End Google Tag Manager -->
+	
 </head>
 <body>
     <header>
@@ -154,6 +162,19 @@ $response->success() && var_dump($response->getData());
         <p>Admissions : 01 81 00 30 33</p>
         <p>admissions@iim.fr</p>
     </footer>
+	
+	<script>
+window.axeptioSettings = {
+  clientId: "62062ea06be0f54d55d66b79",
+};
+ 
+(function(d, s) {
+  var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+  e.async = true; e.src = "//static.axept.io/sdk.js";
+  t.parentNode.insertBefore(e, t);
+})(document, "script");
+</script>
+	
 <script src="script.js"></script>
 </body>
 </html>
